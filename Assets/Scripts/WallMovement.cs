91193088b99a31c5cc86be52c7a639aaeeb4ce62
tyleung <tyleung@ualberta.ca>
2015-10-03 20:08:32 -0600

    added wall movement

---
+++ Assets/Scripts/WallMovement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallMovement : MonoBehaviour
+{
+    // Time in seconds it takes for the wall to reach the robot.
+    float lerpTime = 10f;
+    float currentLerpTime;
+
+    float moveDistance = -30f;
+
+    Vector3 startPos;
+    Vector3 endPos;
+
+    protected void Start()
+    {
+        startPos = transform.position;
+        endPos = transform.position + transform.up * moveDistance;
+    }
+
+    protected void Update()
+    {
+        //reset when we press spacebar
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            currentLerpTime = 0f;
+        }
+
+        //increment timer once per frame
+        currentLerpTime += Time.deltaTime;
+        if (currentLerpTime > lerpTime)
+        {
+            currentLerpTime = lerpTime;
+        }
+
+        //lerp!
+        float perc = currentLerpTime / lerpTime;
+        transform.position = Vector3.Lerp(startPos, endPos, perc);
+    }
+}
